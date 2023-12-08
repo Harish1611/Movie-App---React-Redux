a623c5e13ec113255b915fa6d791f26711988d65
Harish1611 <harishnaidub005@gmail.com>
2023-12-08 20:11:40 +0530

    New: Created component/page routes of app using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header/Header";
+import MovieDetail from "./components/MovieDetail/MovieDetail";
+import Home from "./components/Home/Home";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div>
-      App
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movie/:imdbID" element={<MovieDetail />} />
+          <Route path="*"  element={<PageNotFound />} />
+        </Routes>
+        <Footer />
+      </Router>
+      
     </div>
   );
 }
