fdb4947cd18bd96e05a38f68d56b4afd9c551baa
Harish1611 <harishnaidub005@gmail.com>
2023-12-09 22:53:54 +0530

    New: useSelector is used to get the movies data from store using getAllMovies function

--- src/components/MovieListing/MovieListing.jsx
+++ src/components/MovieListing/MovieListing.jsx
@@ -1,5 +1,9 @@
+import { useSelector } from "react-redux"
+import { getAllMovies } from "../../state/movies/movieSlice"
 
 const MovieListing = () => {
+  const movies = useSelector(getAllMovies);
+  console.log(movies)
   return (
     <div>MovieListing</div>
   )
