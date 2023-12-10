5cca921be3a7c17e63aa17a8fd86c2e4aa65abe3
Harish1611 <harishnaidub005@gmail.com>
2023-12-10 21:53:40 +0530

    New: Added a CSS Spinner/Loader for movie details component

--- src/components/MovieDetail/MovieDetail.jsx
+++ src/components/MovieDetail/MovieDetail.jsx
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom";
 import {
   fetchAsyncMovieOrShowDetail,
   getMovieOrShowDetails,
+  removeSelectedMovieOrShow,
 } from "../../state/movies/movieSlice";
 const MovieDetail = () => {
   const { imdbID } = useParams();
@@ -13,64 +14,95 @@ const MovieDetail = () => {
   console.log(data);
   useEffect(() => {
     dispatch(fetchAsyncMovieOrShowDetail(imdbID));
+
+    // cleanup function to clear the previous moviedetails
+    return () => {
+      dispatch(removeSelectedMovieOrShow());
+    };
   }, [dispatch, imdbID]);
   return (
     <div className="flex justify-evenly px-[40px] py-0 text-fontPrimary font-normal">
-      {/* Left Section */}
-      <div className="">
-        <div className="">
-          <div className="text-[40px] text-fontPrimary">{data.Title}</div>
-          <div className="pl-[3px] mt-[20px] text-fontSecondary flex">
-          <span className="mr-[20px]" >
-            IMDB Rating <i className="fa fa-star text-[#ff9e00]"></i> :{data.imdbRating}
-          </span>
-          <span className="mr-[20px]">
-            IMDB Votes <i className="fa fa-thumbs-up text-[#fafafa]"></i> :{data.imdbVotes}
-          </span>
-          <span className="mr-[20px]">
-            Runtime <i className="fa fa-film text-[#BFD5D6] "></i> :{data.Runtime}
-          </span>
-          <span className="mr-[20px]">
-            Year <i className="fa fa-calendar text-[peachpuff]"></i> :{data.Year}
-          </span>
+      {Object.keys(data).length === 0 ? (
+        <div role="status">
+          <svg
+            aria-hidden="true"
+            class="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+            viewBox="0 0 100 101"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+              fill="currentColor"
+            />
+            <path
+              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+              fill="currentFill"
+            />
+          </svg>
+          <span class="sr-only">Loading...</span>
+        </div>
+      ) : (
+        <>
+          {/* Left Section */}
+          <div className="">
+            <div className="">
+              <div className="text-[40px] text-fontPrimary">{data.Title}</div>
+              <div className="pl-[3px] mt-[20px] text-fontSecondary flex">
+                <span className="mr-[20px]">
+                  IMDB Rating <i className="fa fa-star text-[#ff9e00]"></i> :
+                  {data.imdbRating}
+                </span>
+                <span className="mr-[20px]">
+                  IMDB Votes <i className="fa fa-thumbs-up text-[#fafafa]"></i>{" "}
+                  :{data.imdbVotes}
+                </span>
+                <span className="mr-[20px]">
+                  Runtime <i className="fa fa-film text-[#BFD5D6] "></i> :
+                  {data.Runtime}
+                </span>
+                <span className="mr-[20px]">
+                  Year <i className="fa fa-calendar text-[peachpuff]"></i> :
+                  {data.Year}
+                </span>
+              </div>
+            </div>
+
+            <div className=" mt-[20px] leading-8">{data.Plot}</div>
+            <div className="movie_info">
+              <div className="">
+                <span>Director</span>
+                <span>{data.Director}</span>
+              </div>
+
+              <div className="">
+                <span>Starts</span>
+                <span>{data.Actors}</span>
+              </div>
+
+              <div className="">
+                <span>Generes</span>
+                <span>{data.Genere}</span>
+              </div>
+
+              <div className="">
+                <span>Language</span>
+                <span>{data.Language}</span>
+              </div>
+
+              <div className="">
+                <span>Awards</span>
+                <span>{data.Awards}</span>
+              </div>
+            </div>
           </div>
-        </div>
-        
-        <div className=" mt-[20px] leading-8">{data.Plot}</div>
-        <div className="movie_info">
-        <div className="">
-          <span>Director</span>
-          <span>{data.Director}</span>
-        </div>
 
-        <div className="">
-          <span>Starts</span>
-          <span>{data.Actors}</span>
-        </div>
-
-        <div className="">
-          <span>Generes</span>
-          <span>{data.Genere}</span>
-        </div>
-
-        <div className="">
-          <span>Language</span>
-          <span>{data.Language}</span>
-        </div>
-
-        <div className="">
-          <span>Awards</span>
-          <span>{data.Awards}</span>
-        </div>
-        </div>
-      </div>
-
-
-      {/* Right Section */}
-      <div className="ml-[30px] w-full">
-        <img className="h-full w" src={data.Poster} alt={data.Title} />
-      </div>
-
+          {/* Right Section */}
+          <div className="ml-[30px] w-full">
+            <img className="h-full w" src={data.Poster} alt={data.Title} />
+          </div>
+        </>
+      )}
     </div>
   );
 };
--- src/state/movies/movieSlice.js
+++ src/state/movies/movieSlice.js
@@ -4,92 +4,97 @@ import movieApi from "../../config/movieApi";
 const OMDB_API_KEY = process.env.REACT_APP_OMDB_API_KEY;
 
 // fetch data from OMDB Movie Data using async thunk & axios
-export const fetchAsyncMovies = createAsyncThunk ('movies/fetchAsyncMovies', async () =>{
-
+export const fetchAsyncMovies = createAsyncThunk(
+  "movies/fetchAsyncMovies",
+  async () => {
     const movieText = "Harry";
-    const response = await movieApi.get(`?apiKey=${OMDB_API_KEY}&s=${movieText}&type=movie`);
-    
+    const response = await movieApi.get(
+      `?apiKey=${OMDB_API_KEY}&s=${movieText}&type=movie`
+    );
+
     console.log("Response from API:", response);
 
-    return response.data
-
-});
+    return response.data;
+  }
+);
 
 // fetch data from OMDB show Data using async thunk & axios
-export const fetchAsyncShows = createAsyncThunk ('movies/fetchAsyncShows', async () =>{
-
+export const fetchAsyncShows = createAsyncThunk(
+  "movies/fetchAsyncShows",
+  async () => {
     const seriesText = "Friends";
-    const response = await movieApi.get(`?apiKey=${OMDB_API_KEY}&s=${seriesText}&type=series`);
-    
+    const response = await movieApi.get(
+      `?apiKey=${OMDB_API_KEY}&s=${seriesText}&type=series`
+    );
+
     console.log("Response from API:", response);
 
-    return response.data
-
-});
+    return response.data;
+  }
+);
 
 // fetch data of Movie or Show for Details page using async thunk & axios
-export const fetchAsyncMovieOrShowDetail = createAsyncThunk ('movies/fetchAsyncMovieOrShowDetail', async (id) =>{
+export const fetchAsyncMovieOrShowDetail = createAsyncThunk(
+  "movies/fetchAsyncMovieOrShowDetail",
+  async (id) => {
+    const response = await movieApi.get(
+      `?apiKey=${OMDB_API_KEY}&i=${id}&Plot=full`
+    );
 
-  const response = await movieApi.get(`?apiKey=${OMDB_API_KEY}&i=${id}&Plot=full`);
-  
-  console.log("Response from API:", response);
+    console.log("Response from API:", response);
 
-  return response.data
-
-});
-
+    return response.data;
+  }
+);
 
 // Creating of Initial State
 
-const initialState =  {
-    movies:{},
-    shows:{},
-    selectedMovieOrShow:{},
-}
-
+const initialState = {
+  movies: {},
+  shows: {},
+  selectedMovieOrShow: {},
+};
 
 // Creation of Movie Slice
 
 const movieSlice = createSlice({
-    name: "movies",
-    initialState,
-    reducers:{
-        addMovies:(state, {payload}) => {
-            state.movies = payload 
-        }
+  name: "movies",
+  initialState,
+  reducers: {
+    removeSelectedMovieOrShow: (state) => {
+      state.selectedMovieOrShow = {};
     },
-    extraReducers:(builder) => {
-        builder
-          .addCase(fetchAsyncMovies.pending, () => {
-            console.log("Pending");
-          })
-          .addCase(fetchAsyncMovies.fulfilled, (state, { payload }) => {
-            console.log("Fetched Successfully");
-            return { ...state, movies: payload };
-          })
-          .addCase(fetchAsyncMovies.rejected, () => {
-            console.log("Rejected");
-          })
-          .addCase(fetchAsyncShows.fulfilled, (state, { payload }) => {
-            console.log("Fetched Successfully");
-            return { ...state, shows: payload };
-          })
-          .addCase(fetchAsyncMovieOrShowDetail.fulfilled, (state, { payload }) => {
-            console.log("Fetched Successfully");
-            return { ...state, selectedMovieOrShow: payload };
-          });
-      },
-    
-})
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchAsyncMovies.pending, () => {
+        console.log("Pending");
+      })
+      .addCase(fetchAsyncMovies.fulfilled, (state, { payload }) => {
+        console.log("Fetched Successfully");
+        return { ...state, movies: payload };
+      })
+      .addCase(fetchAsyncMovies.rejected, () => {
+        console.log("Rejected");
+      })
+      .addCase(fetchAsyncShows.fulfilled, (state, { payload }) => {
+        console.log("Fetched Successfully");
+        return { ...state, shows: payload };
+      })
+      .addCase(fetchAsyncMovieOrShowDetail.fulfilled, (state, { payload }) => {
+        console.log("Fetched Successfully");
+        return { ...state, selectedMovieOrShow: payload };
+      });
+  },
+});
 
-export const {addMovies} = movieSlice.actions;
+export const { removeSelectedMovieOrShow } = movieSlice.actions;
 
 // function which returns movies from store
 
 export const getAllMovies = (state) => state.movies.movies;
 export const getAllShows = (state) => state.movies.shows;
-export const getMovieOrShowDetails = (state) => state.movies.selectedMovieOrShow;
-
-
+export const getMovieOrShowDetails = (state) =>
+  state.movies.selectedMovieOrShow;
 
 export default movieSlice.reducer;
